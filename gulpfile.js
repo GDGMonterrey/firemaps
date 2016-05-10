6321b0dbdb0c41ae1f6da5a5555ffdf973591dce
Joel Humberto Gómez Paredes <elmaildeldezkareid@gmail.com>
2016-05-10 00:56:51 -0500

    Added gulp task to transpile jade, less and coffee

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp        = require('gulp'),
+    coffee      = require('gulp-coffee'),
+    jade        = require('gulp-jade'),
+    less        = require('gulp-less');
+
+gulp.task('transpile-jade', function(){
+  return gulp.src("app/jade/index.jade")
+    .pipe(jade())
+    .pipe(gulp.dest('build/'))
+});
+
+gulp.task('transpile-less', function(){
+  return gulp.src("app/less/map.less")
+    .pipe(less())
+    .pipe(gulp.dest('build/css'))
+});
+
+gulp.task('transpile-coffee', function(){
+  return gulp.src("app/coffee/map.coffee")
+    .pipe(coffee())
+    .pipe(gulp.dest('build/js'))
+});
+
+gulp.task('default', ['transpile-jade', 'transpile-less', 'transpile-coffee']);
